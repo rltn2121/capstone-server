137e4fe8ae87eafe766edfbd9f5a61ffc407b3ef
rltn2121 <54628612+rltn2121@users.noreply.github.com>
2021-05-02 05:11:42 +0900

    네이버 소셜로그인

--- src/main/java/com/capstone/mountain/controller/UserController.java
+++ src/main/java/com/capstone/mountain/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.capstone.mountain.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.capstone.mountain.auth.KakaoProfile;
+import com.capstone.mountain.auth.NaverProfile;
 import com.capstone.mountain.auth.OAuthToken;
 import com.capstone.mountain.auth.PrincipalDetails;
 import com.capstone.mountain.domain.User;
@@ -17,6 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.util.LinkedMultiValueMap;
@@ -24,6 +28,9 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -57,7 +64,6 @@ public class UserController{
 
     @GetMapping("/auth/kakao/callback")
     public @ResponseBody Authentication kakaoCallback(String code){
-
         // POST 방식으로 key=value 데이터를 요청 (카카오에게)
         RestTemplate rt = new RestTemplate();
 
@@ -135,13 +141,115 @@ public class UserController{
         // UUID란 -> 중복되지 않는 어떤 특정 값을 만들어내는 알고리즘
         UUID garbagePassword = UUID.randomUUID();
         System.out.println("블로그서버 패스워드 : "+ garbagePassword);
+        return null;
+    }
 
-        String username = kakaoProfile.getKakao_account().getEmail()+"_"+kakaoProfile.getId();
-        String password = garbagePassword.toString();
+//    @GetMapping("/kakao-login")
+//    public @ResponseBody String kakaoLogin(@RequestBody Map<String, String> req, HttpServletResponse response) throws AuthenticationException {
+//        String access_token = req.get("access_token");
+//        RestTemplate rt2 = new RestTemplate();
+//
+//        // HttpHeader 오브젝트 생성
+//        HttpHeaders headers2 = new HttpHeaders();
+//        headers2.add("Authorization", "Bearer "+access_token);
+//        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+//
+//        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+//        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest2 =
+//                new HttpEntity<>(headers2);
+//
+//        // Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
+//        ResponseEntity<String> response2 = rt2.exchange(
+//                "https://kapi.kakao.com/v2/user/me",
+//                HttpMethod.POST,
+//                kakaoProfileRequest2,
+//                String.class
+//        );
+//        System.out.println(response2.getBody());
+//
+//        ObjectMapper objectMapper2 = new ObjectMapper();
+//        KakaoProfile kakaoProfile = null;
+//        try {
+//            kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
+//        } catch (JsonMappingException e) {
+//            e.printStackTrace();
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String username = kakaoProfile.getKakao_account().getEmail()+"_"+kakaoProfile.getId();
+//        String password = "qwerty123";
+//
+//        // 가입자 혹은 비가입자 체크 해서 처리
+//        User originUser = userService.chkUserExist(username);
+//        if(originUser.getUsername() == null) {
+//            System.out.println("기존 회원이 아니기에 자동 회원가입을 진행합니다");
+//            originUser.setUsername(username);
+//            originUser.setPassword(password);
+//            userService.join(originUser);
+//        }
+//
+//        System.out.println("자동 로그인을 진행합니다.");
+//        try{
+//            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(originUser.getUsername(), password);
+//            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+//
+//            // 3. PrincipalDetails를 세션에 담음 (권한 관리를 위해서)
+//            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+//
+//            String jwtToken = JWT.create()
+//                    .withSubject(principalDetails.getUsername())
+//                    .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 120)))
+//                    .withClaim("id", principalDetails.getUser().getId())
+//                    .withClaim("username", principalDetails.getUser().getUsername())
+//                    .sign(Algorithm.HMAC512("cos"));
+//            response.addHeader("Authorization", "Bearer " + jwtToken);
+//            return jwtToken;
+//        } catch(AuthenticationException e){
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
-        User originUser = userService.chkUserExist(username);
+    @GetMapping("/naver-login")
+    public @ResponseBody String naverLogin(@RequestBody Map<String, String> req, HttpServletResponse response) throws AuthenticationException {
+        String access_token = req.get("access_token");
+        RestTemplate rt2 = new RestTemplate();
+
+        // HttpHeader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization", "Bearer "+access_token);
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> naverProfileRequest2 =
+                new HttpEntity<>(headers2);
+
+        // Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
+        ResponseEntity<String> response2 = rt2.exchange(
+            "https://openapi.naver.com/v1/nid/me",
+                HttpMethod.POST,
+                naverProfileRequest2,
+                String.class
+        );
+        System.out.println(response2.getBody());
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        NaverProfile kakaoProfile = null;
+        try {
+            kakaoProfile = objectMapper2.readValue(response2.getBody(), NaverProfile.class);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+        String username = kakaoProfile.getResponse().getEmail()+"_"+kakaoProfile.getResponse().getId();
+        String password = "qwerty123";
+
         // 가입자 혹은 비가입자 체크 해서 처리
-
+        User originUser = userService.chkUserExist(username);
         if(originUser.getUsername() == null) {
             System.out.println("기존 회원이 아니기에 자동 회원가입을 진행합니다");
             originUser.setUsername(username);
@@ -150,21 +258,24 @@ public class UserController{
         }
 
         System.out.println("자동 로그인을 진행합니다.");
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(originUser.getUsername(), originUser.getPassword());
-        System.out.println("11111111111111111111111111");
+        try{
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(originUser.getUsername(), password);
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
-        // authenticationManager로 로그인 시도
-        // -> PrincipalDetailsService가 호출
-        // -> loadUserByUsername() 함수 실행됨
-        Authentication authentication = authenticationManager.authenticate(authenticationToken);
-        System.out.println("2222222222222222222222222222222");
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        System.out.println("33333333333333333333333333333333333");
-//        return authentication;
+            // 3. PrincipalDetails를 세션에 담음 (권한 관리를 위해서)
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
 
-
+            String jwtToken = JWT.create()
+                    .withSubject(principalDetails.getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 120)))
+                    .withClaim("id", principalDetails.getUser().getId())
+                    .withClaim("username", principalDetails.getUser().getUsername())
+                    .sign(Algorithm.HMAC512("cos"));
+            response.addHeader("Authorization", "Bearer " + jwtToken);
+            return jwtToken;
+        } catch(AuthenticationException e){
+            e.printStackTrace();
+        }
         return null;
     }
-
-
 }
