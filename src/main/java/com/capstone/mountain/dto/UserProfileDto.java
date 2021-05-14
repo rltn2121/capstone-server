fdd0457f88886b4e538b37effaf394a2ae021e0d
rltn2121 <54628612+rltn2121@users.noreply.github.com>
2021-05-14 13:16:26 +0900

    프로필 조회 수정

--- src/main/java/com/capstone/mountain/dto/UserProfileDto.java
+++ src/main/java/com/capstone/mountain/dto/UserProfileDto.java
@@ -18,7 +18,8 @@ import static java.lang.Math.round;
 public class UserProfileDto {
     private Long user_id;            // id
     private String nickname;    // 닉네임
-    private String user_height_weight;
+    private String user_height;
+    private String user_weight;
     private String total_distance;  // 총 등산거리
     private String avg_distance;    // 평균 등산거리
     private String total_time;// 총 등산 시간
@@ -37,9 +38,8 @@ public class UserProfileDto {
     public UserProfileDto(Tuple tuple, String duration_total, String duration_avg) {
         this.user_id = tuple.get(0, Long.class);
         this.nickname = tuple.get(1, String.class);
-        this.user_height_weight = (tuple.get(3, Integer.class)==0?"-":tuple.get(3, Integer.class))
-                + "cm / "
-                + (tuple.get(2, Integer.class)==0?"-":tuple.get(2, Integer.class)) + "kg";
+        this.user_height = (tuple.get(3, Integer.class)==0?"-":tuple.get(3, Integer.class)).toString();
+        this.user_weight = (tuple.get(2, Integer.class)==0?"-":tuple.get(2, Integer.class)).toString();
         this.total_distance = (tuple.get(4, Double.class) == null?0.0:round(tuple.get(4, Double.class)*100)/100.0 )+ "km";
         this.avg_distance = (tuple.get(5, Double.class)==null?0.0:round(tuple.get(5, Double.class)*100)/100.0)+ "km";
         this.total_height = (tuple.get(6, Integer.class) == null ? 0 : tuple.get(6, Integer.class))+ "m";
@@ -49,7 +49,12 @@ public class UserProfileDto {
         this.max_speed = (tuple.get(10, Double.class) == null ? 0.0 : tuple.get(10, Double.class))+ "km/h";
         this.total_calories = (tuple.get(11, Integer.class) == null ? 0 : (int)round(tuple.get(11, Integer.class))) + "kcal";
         this.avg_calories = (tuple.get(12, Double.class) == null ? 0 : (int)round(tuple.get(12, Double.class)))+"kcal";
-        this.picture = (tuple.get(13, String.class)==null)?"no_image":tuple.get(13, String.class);
+        String image_url = tuple.get(13, String.class);
+        if(image_url == null || image_url.equals("")){
+            this.picture = "no_image";
+        }
+        else
+            this.picture = image_url;
         this.total_time = duration_total;
         this.avg_time = duration_avg;
     }
